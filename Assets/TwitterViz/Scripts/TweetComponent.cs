4dc3bb3e8d4d805a023e4434f861bf3f944040f1
Yang Liu <lywander@gmail.com>
2017-11-25 23:05:05 -0800

    Surround text
    Copied from Perforce
     Change: 28

--- Assets/TwitterViz/Scripts/TweetComponent.cs
+++ Assets/TwitterViz/Scripts/TweetComponent.cs
@@ -9,11 +9,21 @@ public class TweetComponent : MonoBehaviour
     public TMP_Text WordPrefab;
     public bool Trigger;
     public Tweet Tweet;
-    public float TargetOffset = 20;
 
+    [Header("Animation 1")]
+    public float TargetOffset = 20;
     public float FadeInDuration = 0.3f;
     public float RisingDuration = 1.0f;
     public float FadeOutDuration = 0.2f;
+
+    [Header("Animation Circular")] 
+    public float CircularOffset = 20;
+    public float CircularRadius = 50;
+    public float CircularFadeInDuration = 0.3f;
+    public float CircularRisingDuration = 0.5f;
+    public float CircularFadeOutDuration = 2.0f;
+    public float CircularSpaceWidth = 5;
+    // public float CircularMaxDegree = 90;
 
     [Header("Debugging")]
     [TextArea]
@@ -34,7 +44,7 @@ public class TweetComponent : MonoBehaviour
 	        Trigger = false;
 	        if (!isPlaying)
 	        {
-	            StartCoroutine(wordAnimation());
+	            StartCoroutine(wordAnimationCircular());
 	        }
 	    }	
 	}
@@ -94,18 +104,98 @@ public class TweetComponent : MonoBehaviour
 
                 yield return null;
             }
-            StartCoroutine(wordFadeOut(tmpText));
+            StartCoroutine(wordFadeOut(tmpText, FadeOutDuration));
         }
 
         isPlaying = false;
     }
 
-    private IEnumerator wordFadeOut(TMP_Text word)
+    private IEnumerator wordAnimationCircular()
+    {
+        isPlaying = true;
+
+        List<TMP_Text> textObjects = new List<TMP_Text>();
+        for (int i = 0; i < Tweet.Words.Length; i++)
+        {
+            TMP_Text text = Instantiate(WordPrefab, transform, false);
+            text.text = Tweet.Words[i];
+            text.alpha = 0;
+            textObjects.Add(text);
+        }
+
+        // Wait for one frame for TMP's layout
+        yield return null;
+
+        // Circular layout
+        Vector3 cameraToPointDir = (transform.position - Camera.main.transform.position).normalized;
+        Debug.DrawLine(Camera.main.transform.position, transform.position, Color.yellow, 5);
+        Debug.DrawRay(transform.position, cameraToPointDir * CircularRadius, Color.blue, 5);
+
+        cameraToPointDir.y = 0;
+        float totalWidth = CircularSpaceWidth * (textObjects.Count - 1);
+        for (int i = 0; i < textObjects.Count; i++)
+        {
+            totalWidth += textObjects[i].bounds.extents.x * 2;
+        }
+        float totalDegree = totalWidth / (CircularRadius * Mathf.PI * 2) * 360.0f;
+        float accumulatedWidth = 0;
+        for (int i = 0; i < textObjects.Count; i++)
+        {
+            accumulatedWidth += textObjects[i].bounds.extents.x;
+            float currentDegree = (accumulatedWidth / totalWidth - 0.5f) * totalDegree;
+            Vector3 pointToWordDir = Quaternion.Euler(0, currentDegree, 0) * cameraToPointDir;
+            Debug.DrawRay(transform.position, pointToWordDir * CircularRadius, Color.red, 5);
+
+            Vector3 localPosition = CircularRadius * pointToWordDir;
+            textObjects[i].transform.localPosition = localPosition;
+            textObjects[i].transform.forward = pointToWordDir;
+
+            accumulatedWidth += textObjects[i].bounds.extents.x + CircularSpaceWidth;
+        }
+
+        // Animation
+        for (int i = 0; i < textObjects.Count; i++)
+        {
+            TMP_Text text = textObjects[i];
+            float time = 0;
+            while (time < CircularFadeInDuration || time < CircularRisingDuration)
+            {
+                if (time < CircularFadeInDuration)
+                {
+                    text.alpha = time / CircularFadeInDuration;
+                }
+                else
+                {
+                    text.alpha = 1;
+                }
+
+                Vector3 offset = text.transform.localPosition;
+                if (time < CircularRisingDuration)
+                {
+                    offset.y = Mathf.SmoothStep(0, TargetOffset, time / CircularRisingDuration);
+                }
+                else
+                {
+                    offset.y = TargetOffset;
+                }
+                text.transform.localPosition = offset;
+
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+            StartCoroutine(wordFadeOut(text, CircularFadeOutDuration));
+        }
+
+        isPlaying = false;
+    }
+
+    private IEnumerator wordFadeOut(TMP_Text word, float duration)
     {
         float time = 0;
-        while (time < FadeOutDuration)
+        while (time < duration)
         {
-            word.alpha = 1 - time / FadeOutDuration;
+            word.alpha = 1 - time / duration;
             time += Time.deltaTime;
             yield return null;
         }
