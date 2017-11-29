1cf1c79e91500f922b110da4fc39cfdda614beec
Yang Liu <lywander@gmail.com>
2017-11-28 22:45:02 -0800

    GeoObject works in editor

--- Assets/TwitterViz/Editor/GeoObjectEditor.cs
+++ Assets/TwitterViz/Editor/GeoObjectEditor.cs
@@ -10,27 +10,19 @@ public class GeoObjectEditor : Editor {
     private SerializedProperty longitude;    
     private SerializedProperty altitude;
 
-    void OnEnable()
-    {
-        latitude = serializedObject.FindProperty("Latitude");
-        longitude = serializedObject.FindProperty("Longitude");
-        altitude = serializedObject.FindProperty("Altitude");
-    }
-
     public override void OnInspectorGUI()
     {
-        serializedObject.Update();
-
         GeoObject geoObject = target as GeoObject;
 
         EditorGUI.BeginChangeCheck();
-        double newLat = EditorGUILayout.Slider("Latitude", (float)geoObject.Latitude, -90f, 90);
-        double newLong =  EditorGUILayout.Slider("Longitude", (float)geoObject.Longitude, -180f, 180f);
+        double newLat = EditorGUILayout.DoubleField("Latitude", geoObject.Latitude);
+        double newLong =  EditorGUILayout.DoubleField("Longitude", geoObject.Longitude);
         double newAlt = EditorGUILayout.DoubleField("Altitude", geoObject.Altitude);
         if (EditorGUI.EndChangeCheck())
         {
+            Undo.RecordObject(geoObject, "Change Geo Location");
             geoObject.SetGeoLocation(newLat, newLong, newAlt);
-            serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(geoObject);
         }
     }
 }
--- Assets/TwitterViz/Scripts/Geo/GeoObject.cs
+++ Assets/TwitterViz/Scripts/Geo/GeoObject.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class GeoObject : MonoBehaviour
 {
     [SerializeField]
@@ -17,10 +18,13 @@ public class GeoObject : MonoBehaviour
     private double altitude;
     public double Altitude { get { return altitude; } }
 
+#if UNITY_EDITOR
     private const double GEO_EPSILON = 1e-6;
     private double previousLatitude;
     private double previousLongitude;
     private double previousAltitude;
+    private Vector3 previousPosition;
+#endif
 
 	void Start () 
     {
@@ -29,15 +33,40 @@ public class GeoObject : MonoBehaviour
 	
 	void Update () 
     {
-	    	
-	}
+#if UNITY_EDITOR
+        if (hasGeoLocationChanged())
+        {
+            updateTransformForGeoLocation();
+        }    
+        else if (hasWorldPositionChanged())
+        {
+            updateGeoLocationForTransform();
+        }
+
+        previousLatitude = latitude;
+        previousLongitude = longitude;
+        previousAltitude = altitude;
+        previousPosition = transform.position;
+#endif
+    }
 
     public void SetGeoLocation(double newLatitude, double newLongitude, double newAltitude)
     {
         latitude = newLatitude;
         longitude = newLongitude;
         altitude = newAltitude;
+        
+        updateTransformForGeoLocation();
+    }
+    
+    public void SetWorldPosition(Vector3 position)
+    {
+        transform.position = position;
+        updateGeoLocationForTransform();
+    }
 
+    private void updateTransformForGeoLocation()
+    {
         MapModel mapModel = GetComponentInParent<MapModel>();
         if (mapModel == null)
         {
@@ -47,13 +76,7 @@ public class GeoObject : MonoBehaviour
         transform.position = mapModel.EarthToUnityWorld(latitude, longitude, altitude);
     }
 
-    public void SetWorldPosition(Vector3 position)
-    {
-        transform.position = position;
-        UpdateGeoLocationForTransform();
-    }
-
-    public void UpdateGeoLocationForTransform()
+    private void updateGeoLocationForTransform()
     {
         MapModel mapModel = GetComponentInParent<MapModel>();
         if (mapModel == null)
@@ -64,4 +87,18 @@ public class GeoObject : MonoBehaviour
         
         mapModel.UnityWorldToEarth(transform.position, out latitude, out longitude, out altitude);
     }
+
+#if UNITY_EDITOR
+    private bool hasGeoLocationChanged()
+    {
+        return Math.Abs(latitude - previousLatitude) > GEO_EPSILON
+               || Math.Abs(longitude - previousLongitude) > GEO_EPSILON
+               || Math.Abs(altitude - previousAltitude) > GEO_EPSILON;
+    }
+
+    private bool hasWorldPositionChanged()
+    {
+        return transform.position != previousPosition;
+    }
+#endif
 }
