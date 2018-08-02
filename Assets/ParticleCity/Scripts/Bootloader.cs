db9c8050c058d387d4f3ec9340dfcd80cdb3cb99
Yang Liu <lywander@gmail.com>
2018-08-01 23:45:21 -0700

    cp

---
+++ Assets/ParticleCity/Scripts/Bootloader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class Bootloader : MonoBehaviour
+{
+    private AsyncOperation citySceneLoadOp;
+
+	void Start () {
+        Debug.Log("Loaded XR Device: " + XRSettings.loadedDeviceName);
+
+	    if (XRSettings.loadedDeviceName == "Oculus")
+	    {
+	        SceneManager.LoadScene("oculus_rift_components");
+	    }
+        else if (XRSettings.loadedDeviceName == "OpenVR")
+	    {
+            SceneManager.LoadScene("steam_vr_components");
+	    }
+
+	    citySceneLoadOp = SceneManager.LoadSceneAsync("new_york_scene", LoadSceneMode.Additive);
+	    citySceneLoadOp.allowSceneActivation = true;
+	}
+	
+	void Update () {
+	    if (citySceneLoadOp.isDone)
+	    {
+	        SceneManager.UnloadSceneAsync("bootloader");
+	    }
+	}
+}
